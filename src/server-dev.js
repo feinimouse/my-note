587fe8a293e8e3ebc1e308b18f7654b1311a14b4
feinimouse <704602977@qq.com>
2019-10-03 11:53:12 +0800

    设置开发目录

--- src/server-dev.js
+++ src/server-dev.js
@@ -4,7 +4,7 @@ const route = require('koa-route');
 const path = require('path');
 
 const app = new KOA();
-const source = koaStatic(path.join(__dirname, '../publish'));
+const source = koaStatic(path.join(__dirname, '../dev'));
 
 app.use(source);
 app.use(route.get('/', (ctx) => {
