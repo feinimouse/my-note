af7679a82cf47856fcf77a11c50c0e563432630f
feinimouse <704602977@qq.com>
2019-10-06 22:47:21 +0800

    await缺失的bug

--- src/test/testCreateFile.js
+++ src/test/testCreateFile.js
@@ -1,21 +1,25 @@
 const fs = require('fs').promises;
 const $path = require('path');
 
+const createFile = require('../util/createFile');
+
 const path = $path.resolve(__dirname, './a/b/c/d/e.html');
-fs.writeFile(path, 'qwer')
-    .catch(async e => {
-        // console.log(e);
-        const { root, dir } = $path.parse(path);
-        const exec = async dirName => {
-            if (dirName !== root) {
-                try {
-                    await fs.mkdir(dirName);
-                } catch (err) {
-                    await exec($path.dirname(dirName));
-                    await fs.mkdir(dirName);
-                }
-            }
-        };
-        await exec(dir);
-        await fs.writeFile(path, 'qwer');
-    });
+// fs.writeFile(path, 'qwer')
+//     .catch(async e => {
+//         // console.log(e);
+//         const { root, dir } = $path.parse(path);
+//         const exec = async dirName => {
+//             if (dirName !== root) {
+//                 try {
+//                     await fs.mkdir(dirName);
+//                 } catch (err) {
+//                     await exec($path.dirname(dirName));
+//                     await fs.mkdir(dirName);
+//                 }
+//             }
+//         };
+//         await exec(dir);
+//         await fs.writeFile(path, 'qwer');
+//     });
+
+createFile(path, 'qwer').catch(e => console.log(e));
--- src/util/createFile.js
+++ src/util/createFile.js
@@ -3,24 +3,26 @@ const fs = require('fs').promises;
 
 module.exports = async (path, content) => {
     try {
-        fs.writeFile(path, content);
+        await fs.writeFile(path, content);
     } catch (err) {
-        if (err.code === 'ENOENT') {
-            const { root, dir } = $path.parse(path);
-            const mk = async dirName => {
-                if (dirName !== root) {
-                    try {
-                        await fs.mkdir(dirName);
-                    } catch (e) {
-                        if (e.code === 'ENOENT') {
-                            await mk($path.dirname(dirName));
-                            await fs.mkdir(dirName);
-                        }
-                    }
-                }
-            };
-            await mk(dir);
-            await fs.writeFile(path, content);
+        if (!err.code === 'ENOENT') {
+            throw err;
         }
+        const { root, dir } = $path.parse(path);
+        const mk = async dirName => {
+            if (dirName !== root) {
+                try {
+                    await fs.mkdir(dirName);
+                } catch (e) {
+                    if (!e.code === 'ENOENT') {
+                        throw e;
+                    }
+                    await mk($path.dirname(dirName));
+                    await fs.mkdir(dirName);
+                }
+            }
+        };
+        await mk(dir);
+        await fs.writeFile(path, content);
     }
 };
