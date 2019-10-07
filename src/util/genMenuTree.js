23de7366f659735809006818f3940be7c4ade424
feinimouse <704602977@qq.com>
2019-10-07 20:43:01 +0800

    将生成目录工具的配置单独提出

--- src/util/genMenuTree.js
+++ src/util/genMenuTree.js
@@ -1,31 +1,13 @@
-
-const $path = require('path');
 const { findFileTree } = require('./findFile');
 
-module.exports = async (articleFolder = './', articleTest = /\.md$/) => {
+module.exports = async (path = './', test = /\.md$/, exProps = {}) => {
     // 将所有文章整理到一个数组中
     const articles = [];
     // 生成菜单树
     const catalog = await findFileTree({
-        path: articleFolder,
-        test: articleTest,
-        exProps: {
-            id({ sort }) {
-                return sort.join('-');
-            },
-            rootsId({ sort, dir }) {
-                return dir && Array.isArray(dir.rootsId)
-                    ? dir.rootsId.concat(sort.join('-'))
-                    : [sort.join('-')];
-            },
-            url({ path, isFolder, sort }) {
-                const name = $path.basename(path);
-                if (!isFolder) {
-                    return `${name.replace('.md', '')}${sort.join('')}.html`;
-                }
-                return undefined;
-            },
-        },
+        path,
+        test,
+        exProps,
         async onFind(article) {
             articles.push(article);
         },
