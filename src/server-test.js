aea219221a67547728cce9bc5a388606dbf088cc
feinimouse <704602977@qq.com>
2019-10-29 15:39:11 +0800

    本地测试服务器

---
+++ src/server-test.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const KOA = require('koa');
+const koaStatic = require('koa-static');
+const route = require('koa-route');
+const path = require('path');
+
+const app = new KOA();
+const source = koaStatic(path.join(__dirname, '../build'));
+
+app.use(source);
+app.use(route.get('/', ctx => {
+    ctx.response.redirect('./index.html');
+}));
+
+app.listen(3000);
+console.log('listening port: 3000 ...');
