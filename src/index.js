ce38c2ce710d708a56c93e250f28812a7933d6ee
feinimouse <704602977@qq.com>
2019-10-29 13:16:48 +0800

    添加输出文字

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const genMenuTree = require('./util/genMenuTree');
 const parseMenu = require('./util/parseMenu');
 const createMdHtml = require('./util/createMdHtml');
 const { createFile } = require('./util/createFile');
+const copyFile = require('./util/copyFile');
 
 const configDev = require('./config.dev');
 
@@ -9,22 +10,33 @@ const run = async config => {
     const {
         input, test, template, exProps, output,
     } = config;
+    console.log(`\n input: ${input} \n output: ${output} \n template: ${template} \n`);
     // 初始化模板生成器
     await createMdHtml.init(template);
+    console.log('template has been read...');
     // 生成目录信息，和扫描到的文章列表
     const { catalog, articles } = await genMenuTree(input, test, exProps);
+    console.log('md articles list has been found...');
     // 生成目录html
     const menu = parseMenu(catalog);
     // 生成首页
-    const home = createMdHtml.fromStr('', { ...config, menu, title: 'Home', rootsId: '[]' });
-    createFile(`${output}/index.html`, home);
+    const home = await createMdHtml.fromStr('欢迎来到菲尼莫斯的博客', { ...config, menu, title: 'Home', rootsId: '[]' });
+    await createFile(`${output}/index.html`, home);
+    console.log('home has been created...');
     // 生成子页面
     await Promise.all(articles.map(async art => {
         const html = await createMdHtml.fromFile(art.path, { ...config, ...art, menu });
-        createFile(art.output, html);
+        await createFile(art.output, html);
+        console.log(`${art.path} has been created...`);
     }));
+    // 复制静态文件
+    await copyFile.copyTestFile();
+    console.log('static file has been copied...');
 };
 
-if (process.env === 'development') {
-    run(configDev);
+console.log('build start...');
+// console.log(JSON.stringify(process.env.NODE_ENV));
+if (process.env.NODE_ENV === 'development') {
+    console.log('use dev config...');
+    run(configDev).then(() => console.log('build has been finished...'));
 }
