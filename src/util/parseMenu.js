6ded028c6ab61203a910675973debe67aab227fa
feinimouse <704602977@qq.com>
2019-10-30 15:31:01 +0800

    多余的注释

--- src/util/parseMenu.js
+++ src/util/parseMenu.js
@@ -1,32 +1,3 @@
-/**
- * 将输入的json菜单转为html
- * @param {*} menuArr 菜单的树状表达
- * 输入样例：[
-    {
-        title: 'JAVA开发',
-        id: '1',
-        children: [
-            {
-                title: 'JAVA多线程',
-                id: '1-1',
-                url: '1-1.html',
-            },
-            {
-                title: 'JAVA虚拟机',
-                id: '1-2',
-                children: [
-                    {
-                        title: '内存',
-                        id: '1-2-1',
-                        url: '1-2-1.html',
-                    },
-                ],
-            },
-        ],
-    },
-];
- */
-
 const childMenu = ({ id, title, childHtml }) => `<li data-id="${id}">
  <span>${title}</span>
  <ul>
